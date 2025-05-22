5ccd52751328c4388c872d01edda407fe3790adf
Yatsenko Pavlo <pavelyasenko18@gmail.com>
2025-05-22 20:34:56 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write code here
+document.addEventListener('click', () => {
+  const titles = [...document.querySelectorAll('.title')];
+
+  titles.forEach((title) => {
+    const childElement = title.nextElementSibling;
+
+    title.style.cursor = 'pointer';
+
+    if (childElement && childElement.tagName === 'ul') {
+      title.style.display = 'none';
+    }
+
+    title.addEventListener('click', (e) => {
+      // eslint-disable-next-line no-constant-condition
+      childElement.style.display =
+        childElement.style.display === 'none' ? 'block' : 'none';
+
+      e.stopPropagation();
+    });
+  });
+});
