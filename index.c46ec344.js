874684ec5c69479d3f8a054838fbb1503d4b4dac
Yatsenko Pavlo <pavelyasenko18@gmail.com>
2025-06-05 14:47:22 +0300

    make build

---
+++ index.c46ec344.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(e){document.querySelectorAll(".title").forEach(function(e){var n=e.nextElementSibling;n&&"ul"===n.tagName&&(e.style.display="none"),e.addEventListener("click",function(){n.style.display="none"===n.style.display?"block":"none"})})});
+//# sourceMappingURL=index.c46ec344.js.map
